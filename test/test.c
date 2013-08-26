17669c2c2b810b7d37445ae3f09e979e8411cde8
cquaid <codequaid@gmail.com>
2013-08-26 13:13:57 -0500

    Add getopt loop, usage and verboseity to testprog

--- test/test.c
+++ test/test.c
@@ -1,12 +1,16 @@
 #include <sys/types.h>
 
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "../tap_parser.h"
+
+/* Globals */
+static int verbose = 0;
 
 /* Callback Functions */
 static int invalid(tap_parser *tp, int err, const char *msg);
@@ -22,22 +26,61 @@ static int test(tap_parser *tp, tap_test_result *ttr);
 static void dump_tap_stats(tap_parser *tp);
 static pid_t exec_test(tap_parser *tp, const char *path);
 
+static void vprint(const char *fmt, ...);
+
+
+static void
+usage(FILE *file, const char *name)
+{
+    fprintf(file, "usage: %s [options] filename\n", name);
+    fprintf(file, " -h    display this message\n");
+    fprintf(file, " -v    increase verbose output\n");
+    fflush(file);
+}
+
 int
 main(int argc, char *argv[])
 {
+    int opt;
     pid_t pid;
     tap_parser tp;
 
+    const char *name;
+    const char *filename;
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <filename>\n", argv[0]);
-        return 1;
+    name = argv[0];
+
+    while ((opt = getopt(argc, argv, "vh")) != EOF) {
+        switch (opt) {
+        case 'v':
+            verbose++;
+            break;
+        case 'h':
+            usage(stdout, name);
+            exit(EXIT_SUCCESS);
+        default:
+            fprintf(stderr, "Invalid option: %c\n", (char)(opt & 0xff));
+            usage(stderr, name);
+            exit(EXIT_FAILURE);
+        }
     }
+
+    argv += optind;
+    argc -= optind;
+
+    if (argc != 1) {
+        fprintf(stderr, "Missing filename!");
+        usage(stderr, name);
+        exit(EXIT_FAILURE);
+    }
+
+    filename = argv[0];
+    vprint("Running %s\n", filename);
 
     tap_parser_init(&tp, 512);
 
-    pid = exec_test(&tp, argv[1]);
-    printf("Child pid: %lu\n", (unsigned long)pid);
+    pid = exec_test(&tp, filename);
+    vprint("Child pid: %lu\n", (unsigned long)pid);
 
     tap_parser_set_test_callback(&tp, test);
     tap_parser_set_plan_callback(&tp, plan);
@@ -72,7 +115,10 @@ dump_tap_stats(tap_parser *tp)
             printf("All Skipped.\n\n");
     }
 
-    printf("Ran %ld of %ld tests:\n", tp->test_num, tp->plan);
+    if (tp->plan == -1)
+        printf("Ran %ld of 0 tests.\n", tp->test_num);
+    else
+        printf("Ran %ld of %ld tests.\n", tp->test_num, tp->plan);
     printf("    Tap Version: %ld\n", tp->version);
     printf("     Tap Errors: %ld\n", tp->parse_errors);
     printf("         Failed: %ld\n", tp->failed);
@@ -132,12 +178,27 @@ exec_test(tap_parser *tp, const char *path)
 #undef WRITE_PIPE
 }
 
+static void
+vprint(const char *fmt, ...)
+{
+    va_list ap;
+
+    if (!verbose) return;
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    fflush(stderr);
+}
+
+
+/* TAP Functions */
 
 static int
 invalid(tap_parser *tp, int err, const char *msg)
 {
-    fprintf(stderr, "Error: [%d] %s\n", err, msg);
-    fflush(stderr);
+    vprint("Error: [%d] %s\n", err, msg);
 
     return tap_default_invalid_callback(tp, err, msg);
 }
@@ -152,8 +213,7 @@ unknown(tap_parser *tp)
     if (tp->buffer[len - 1] == '\n')
         tp->buffer[len - 1] = '\0';
 
-    fprintf(stderr, "Unknown: %s\n", tp->buffer);
-    fflush(stderr);
+    vprint("Unknown: %s\n", tp->buffer);
 
     return tap_default_unknown_callback(tp);
 }
@@ -161,8 +221,7 @@ unknown(tap_parser *tp)
 static int
 version(tap_parser *tp, long tap_version)
 {
-    printf("Version: %ld\n", tap_version);
-    fflush(stdout);
+    vprint("Version: %ld\n", tap_version);
 
     return tap_default_version_callback(tp, tap_version);
 }
@@ -186,13 +245,11 @@ comment(tap_parser *tp)
 static int
 bailout(tap_parser *tp, char *msg)
 {
-    printf("Bail out!");
+    vprint("Bail out!");
     if (msg)
-        printf(" %s\n", msg);
+        vprint(" %s\n", msg);
     else
-        putchar('\n');
-
-    fflush(stdout);
+        vprint("\n");
 
     return tap_default_bailout_callback(tp, msg);
 }
@@ -200,8 +257,7 @@ bailout(tap_parser *tp, char *msg)
 static int
 pragma(tap_parser *tp, int state, char *pragma)
 {
-    printf("Pragma: %c%s\n", (state) ? '+' : '-', pragma);
-    fflush(stdout);
+    vprint("Pragma: %c%s\n", (state) ? '+' : '-', pragma);
 
     return tap_default_pragma_callback(tp, state, pragma);
 }
@@ -209,13 +265,11 @@ pragma(tap_parser *tp, int state, char *pragma)
 static int
 plan(tap_parser *tp, long upper, char *skip)
 {
-    printf("Plan: 1..%ld", upper);
+    vprint("Plan: 1..%ld", upper);
     if (skip)
-        printf(" # skip %s\n", skip);
+        vprint(" # skip %s\n", skip);
     else
-        putchar('\n');
-
-    fflush(stdout);
+        vprint("\n");
 
     return tap_default_plan_callback(tp, upper, skip);
 }
@@ -223,40 +277,38 @@ plan(tap_parser *tp, long upper, char *skip)
 static int
 test(tap_parser *tp, tap_test_result *ttr)
 {
-    printf("Test: %ld ", ttr->test_num);
+    vprint("Test: %ld ", ttr->test_num);
     switch (ttr->type) {
     case TTT_OK:
-        printf("ok");
+        vprint("ok");
         break;
     case TTT_NOT_OK:
-        printf("not ok");
+        vprint("not ok");
         break;
     case TTT_TODO:
-        printf("todo");
+        vprint("todo");
         break;
     case TTT_TODO_PASSED:
-        printf("ok todo");
+        vprint("ok todo");
         break;
     case TTT_SKIP:
-        printf("skip");
+        vprint("skip");
         break;
     case TTT_SKIP_FAILED:
-        printf("not ok skip");
+        vprint("not ok skip");
         break;
     }
 
     if (ttr->reason) {
         if (ttr->directive)
-            printf(": %s (%s)\n", ttr->reason, ttr->directive);
+            vprint(": %s (%s)\n", ttr->reason, ttr->directive);
         else
-            printf(": %s\n", ttr->reason);
+            vprint(": %s\n", ttr->reason);
     }
     else if (ttr->directive)
-        printf(" (%s)\n", ttr->directive);
+        vprint(" (%s)\n", ttr->directive);
     else
-        putchar('\n');
-
-    fflush(stdout);
+        vprint("\n");
 
     return tap_default_test_callback(tp, ttr);
 }
