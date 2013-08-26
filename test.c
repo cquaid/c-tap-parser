df42f3fd968e591897b51c432c9677b7efb18876
Joshua Dickson <joshua.dickson@pikewerks.com>
2013-08-26 14:55:48 -0500

    Fix the testprog a bit...

--- test.c
+++ test.c
@@ -10,7 +10,7 @@
 #include "tap_parser.h"
 
 /* Globals */
-static int verbose = 0;
+static int verbosity = 0;
 
 /* Callback Functions */
 static int invalid(tap_parser *tp, int err, const char *msg);
@@ -24,9 +24,8 @@ static int test(tap_parser *tp, tap_test_result *ttr);
 
 /* Helpers */
 static void dump_tap_stats(tap_parser *tp);
+static void analyze_results(tap_parser *tp);
 static pid_t exec_test(tap_parser *tp, const char *path);
-
-static void vprint(const char *fmt, ...);
 
 
 static void
@@ -41,6 +40,7 @@ usage(FILE *file, const char *name)
 int
 main(int argc, char *argv[])
 {
+    int ret;
     int opt;
     pid_t pid;
     tap_parser tp;
@@ -53,7 +53,7 @@ main(int argc, char *argv[])
     while ((opt = getopt(argc, argv, "vh")) != EOF) {
         switch (opt) {
         case 'v':
-            verbose++;
+            verbosity++;
             break;
         case 'h':
             usage(stdout, name);
@@ -75,12 +75,23 @@ main(int argc, char *argv[])
     }
 
     filename = argv[0];
-    vprint("Running %s\n", filename);
+    if (verbosity >= 3) {
+        printf("Running %s\n", filename);
+        fflush(stdout);
+    }
 
-    tap_parser_init(&tp, 512);
+    ret = tap_parser_init(&tp, 512);
+    if (ret != 0) {
+        fprintf(stderr, "tap_parser_init(): %s\n", strerror(ret));
+        fflush(stderr);
+        exit(EXIT_FAILURE);
+    }
 
     pid = exec_test(&tp, filename);
-    vprint("Child pid: %lu\n", (unsigned long)pid);
+    if (verbosity >= 3) {
+        printf("Child pid: %lu\n", (unsigned long)pid);
+        fflush(stdout);
+    }
 
     tap_parser_set_test_callback(&tp, test);
     tap_parser_set_plan_callback(&tp, plan);
@@ -94,7 +105,10 @@ main(int argc, char *argv[])
     while (tap_parser_next(&tp) == 0)
         ;
 
-    dump_tap_stats(&tp);
+    if (verbosity >= 3)
+        dump_tap_stats(&tp);
+
+    analyze_results(&tp);
 
     tap_parser_fini(&tp);
     close(tp.fd);
@@ -116,9 +130,9 @@ dump_tap_stats(tap_parser *tp)
     }
 
     if (tp->plan == -1)
-        printf("Ran %ld of 0 tests.\n", tp->test_num);
+        printf("Ran %ld of 0 tests.\n", tp->tests_run);
     else
-        printf("Ran %ld of %ld tests.\n", tp->test_num, tp->plan);
+        printf("Ran %ld of %ld tests.\n", tp->tests_run, tp->plan);
     printf("    Tap Version: %ld\n", tp->version);
     printf("     Tap Errors: %ld\n", tp->parse_errors);
     printf("         Failed: %ld\n", tp->failed);
@@ -128,6 +142,47 @@ dump_tap_stats(tap_parser *tp)
     printf("        Skipped: %ld\n", tp->skipped);
     printf("          Todos: %ld\n", tp->todo);
     printf("  Dubious Todos: %ld\n", tp->todo_passed);
+
+    fflush(stdout);
+}
+
+static void
+analyze_results(tap_parser *tp)
+{
+    putchar('\n');
+
+    if (tp->bailed)
+        printf("Aborted (Bailed Out)\n");
+    else if (tp->plan == -1) {
+        tp->plan = 0;
+        printf("Aborted (No Plan)\n");
+    }
+    else if (tp->tests_run > tp->plan)
+        printf("Aborted (Extra Tests)\n");
+    else if (tp->tests_run < tp->plan)
+        printf("Aborted (Missing Tests)\n");
+
+    if (tp->failed > 0)
+        printf("Failed %ld/%ld tests", tp->failed, tp->tests_run);
+    else
+        printf("All tests successful");
+
+    if (tp->skipped > 0) {
+        if (tp->skipped == 1)
+            printf(", 1 test skipped");
+        else
+            printf(", %ld tests skipped", tp->skipped);
+    }
+
+    printf(".\n");
+    if (tp->parse_errors > 0) {
+        printf("%ld TAP error", tp->parse_errors);
+         if (tp->parse_errors != 1)
+                putchar('s');
+        putchar('\n');
+    }
+
+    fflush(stdout);
 }
 
 static pid_t
@@ -178,27 +233,16 @@ exec_test(tap_parser *tp, const char *path)
 #undef WRITE_PIPE
 }
 
-static void
-vprint(const char *fmt, ...)
-{
-    va_list ap;
-
-    if (!verbose) return;
-
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    va_end(ap);
-
-    fflush(stderr);
-}
-
 
 /* TAP Functions */
 
 static int
 invalid(tap_parser *tp, int err, const char *msg)
 {
-    vprint("Error: [%d] %s\n", err, msg);
+    if (verbosity >= 3) {
+        printf("Error: [%d] %s\n", err, msg);
+        fflush(stdout);
+    }
 
     return tap_default_invalid_callback(tp, err, msg);
 }
@@ -208,12 +252,16 @@ unknown(tap_parser *tp)
 {
     size_t len;
 
+    if (verbosity < 3)
+        return tap_default_unknown_callback(tp);
+
     len = strlen(tp->buffer);
     /* remove the trailing newline */
     if (tp->buffer[len - 1] == '\n')
         tp->buffer[len - 1] = '\0';
 
-    vprint("Unknown: %s\n", tp->buffer);
+    printf("Unknown: %s\n", tp->buffer);
+    fflush(stdout);
 
     return tap_default_unknown_callback(tp);
 }
@@ -221,7 +269,10 @@ unknown(tap_parser *tp)
 static int
 version(tap_parser *tp, long tap_version)
 {
-    vprint("Version: %ld\n", tap_version);
+    if (verbosity >= 3) {
+        printf("Version: %ld\n", tap_version);
+        fflush(stdout);
+    }
 
     return tap_default_version_callback(tp, tap_version);
 }
@@ -230,6 +281,9 @@ static int
 comment(tap_parser *tp)
 {
     size_t len;
+
+    if (verbosity < 3)
+        return tap_default_comment_callback(tp);
 
     len = strlen(tp->buffer);
     /* remove the trailing newline */
@@ -245,11 +299,16 @@ comment(tap_parser *tp)
 static int
 bailout(tap_parser *tp, char *msg)
 {
-    vprint("Bail out!");
+    if (verbosity < 3)
+        return tap_default_bailout_callback(tp, msg);
+
+    printf("Bail out!");
     if (msg)
-        vprint(" %s\n", msg);
+        printf(" %s\n", msg);
     else
-        vprint("\n");
+        putchar('\n');
+
+    fflush(stdout);
 
     return tap_default_bailout_callback(tp, msg);
 }
@@ -257,7 +316,10 @@ bailout(tap_parser *tp, char *msg)
 static int
 pragma(tap_parser *tp, int state, char *pragma)
 {
-    vprint("Pragma: %c%s\n", (state) ? '+' : '-', pragma);
+    if (verbosity >= 3) {
+        printf("Pragma: %c%s\n", (state) ? '+' : '-', pragma);
+        fflush(stdout);
+    }
 
     return tap_default_pragma_callback(tp, state, pragma);
 }
@@ -265,11 +327,16 @@ pragma(tap_parser *tp, int state, char *pragma)
 static int
 plan(tap_parser *tp, long upper, char *skip)
 {
-    vprint("Plan: 1..%ld", upper);
+    if (verbosity < 3)
+            return tap_default_plan_callback(tp, upper, skip);
+
+    printf("Plan: 1..%ld", upper);
     if (skip)
-        vprint(" # skip %s\n", skip);
+        printf(" # skip %s\n", skip);
     else
-        vprint("\n");
+        putchar('\n');
+
+    fflush(stdout);
 
     return tap_default_plan_callback(tp, upper, skip);
 }
@@ -277,38 +344,43 @@ plan(tap_parser *tp, long upper, char *skip)
 static int
 test(tap_parser *tp, tap_test_result *ttr)
 {
-    vprint("Test: %ld ", ttr->test_num);
+    if (verbosity < 3)
+        return tap_default_test_callback(tp, ttr);
+
+    printf("Test: %ld ", ttr->test_num);
     switch (ttr->type) {
     case TTT_OK:
-        vprint("ok");
+        printf("ok");
         break;
     case TTT_NOT_OK:
-        vprint("not ok");
+        printf("not ok");
         break;
     case TTT_TODO:
-        vprint("todo");
+        printf("todo");
         break;
     case TTT_TODO_PASSED:
-        vprint("ok todo");
+        printf("ok todo");
         break;
     case TTT_SKIP:
-        vprint("skip");
+        printf("skip");
         break;
     case TTT_SKIP_FAILED:
-        vprint("not ok skip");
+        printf("not ok skip");
         break;
     }
 
     if (ttr->reason) {
         if (ttr->directive)
-            vprint(": %s (%s)\n", ttr->reason, ttr->directive);
+            printf(": %s (%s)\n", ttr->reason, ttr->directive);
         else
-            vprint(": %s\n", ttr->reason);
+            printf(": %s\n", ttr->reason);
     }
     else if (ttr->directive)
-        vprint(" (%s)\n", ttr->directive);
+        printf(" (%s)\n", ttr->directive);
     else
-        vprint("\n");
+        putchar('\n');
+
+    fflush(stdout);
 
     return tap_default_test_callback(tp, ttr);
 }
